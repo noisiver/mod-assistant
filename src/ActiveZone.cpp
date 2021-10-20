a37b4f13738ad250e818482f1b9f5117c0f23a15
Revision <tkn963@gmail.com>
2021-10-20 03:50:17 +0200

    Active zone
    Updated the check for active creatures to hopefully only trigger setActive once instead of setting it over and over for no reason.
    
    I'm honestly not sure how effective this is, but hopefully it'll at least make it feel a little more alive without sacrificing processing power.

--- src/ActiveZone.cpp
+++ src/ActiveZone.cpp
@@ -1,5 +1,13 @@
 #include "Config.h"
 
+class ActiveCreature : public DataMap::Base
+{
+    public:
+        ActiveCreature() {}
+        ActiveCreature(bool isActive) : isActive(isActive) {}
+        bool isActive = false;
+};
+
 class ActiveZone : public AllCreatureScript
 {
     public:
@@ -8,22 +16,36 @@ class ActiveZone : public AllCreatureScript
         void OnAllCreatureUpdate(Creature* creature, uint32 diff) override
         {
             if (sConfigMgr->GetBoolDefault("Assistant.Creature.SetActive", 0))
-            {
                 if (sWorld->getBoolConfig(CONFIG_PRELOAD_ALL_NON_INSTANCED_MAP_GRIDS))
+                    SetActiveCreature(creature);
+        }
+
+    private:
+        void SetActiveCreature(Creature* creature)
+        {
+            if (!creature)
+                return;
+
+            if (creature->GetMapId() != 0 && creature->GetMapId() != 1 && creature->GetMapId() != 530 && creature->GetMapId() != 571)
+                return;
+
+            if (!creature->GetZoneId())
+                return;
+
+            if (sWorld->FindPlayerInZone(creature->GetZoneId()))
+            {
+                if (!creature->CustomData.GetDefault<ActiveCreature>("ActiveCreature")->isActive)
                 {
-                    if (creature->GetMapId() == 0 || creature->GetMapId() == 1 || creature->GetMapId() == 530 || creature->GetMapId() == 571)
-                    {
-                        if (sWorld->FindPlayerInZone(creature->GetZoneId()))
-                        {
-                            if (!creature->isActiveObject())
-                                creature->setActive(true);
-                        }
-                        else
-                        {
-                            if (creature->isActiveObject())
-                                creature->setActive(false);
-                        }
-                    }
+                    creature->CustomData.Set("ActiveCreature", new ActiveCreature(creature->GetGUID()));
+                    creature->setActive(true);
+                }
+            }
+            else
+            {
+                if (creature->CustomData.GetDefault<ActiveCreature>("ActiveCreature")->isActive)
+                {
+                    creature->CustomData.GetDefault<ActiveCreature>("ActiveCreature")->isActive = false;
+                    creature->setActive(false);
                 }
             }
         }
