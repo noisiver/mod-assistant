fdeeddd6ce437101ac017938483a610b696bc6df
Revision <tkn963@gmail.com>
2021-11-19 20:30:46 +0100

    Fixed
    Fixed an issue using the weekend bonus. The current day of the week only gets set upon startup. This should hopefully let it change day of the week without having to restart the server.

--- src/WeekendBonus.cpp
+++ src/WeekendBonus.cpp
@@ -1,9 +1,6 @@
 #include "Player.h"
 #include "Config.h"
 
-time_t t = time(NULL);
-tm* now = localtime(&t);
-
 class WeekendBonus : public PlayerScript
 {
     public:
@@ -12,23 +9,26 @@ class WeekendBonus : public PlayerScript
         void OnGiveXP(Player* player, uint32& amount, Unit* /*victim*/) override
         {
             if (sConfigMgr->GetBoolDefault("Assistant.Rate.Weekend.Enabled", 0))
-                if (now->tm_wday == 5 /*Friday*/ || now->tm_wday == 6 /*Saturday*/ || now->tm_wday == 0 /*Sunday*/)
+                if (localtime(&t)->tm_wday == 5 /*Friday*/ || localtime(&t)->tm_wday == 6 /*Saturday*/ || localtime(&t)->tm_wday == 0 /*Sunday*/)
                     amount *= 2;
         }
 
         void OnReputationChange(Player* player, uint32 /*factionId*/, int32& standing, bool /*incremental*/) override
         {
             if (sConfigMgr->GetBoolDefault("Assistant.Rate.Weekend.Enabled", 0))
-                if (now->tm_wday == 5 /*Friday*/ || now->tm_wday == 6 /*Saturday*/ || now->tm_wday == 0 /*Sunday*/)
+                if (localtime(&t)->tm_wday == 5 /*Friday*/ || localtime(&t)->tm_wday == 6 /*Saturday*/ || localtime(&t)->tm_wday == 0 /*Sunday*/)
                     standing *= 2;
         }
 
         void OnLogin(Player* player) override
         {
             if (sConfigMgr->GetBoolDefault("Assistant.Rate.Weekend.Enabled", 0))
-                if (now->tm_wday == 5 /*Friday*/ || now->tm_wday == 6 /*Saturday*/ || now->tm_wday == 0 /*Sunday*/)
+                if (localtime(&t)->tm_wday == 5 /*Friday*/ || localtime(&t)->tm_wday == 6 /*Saturday*/ || localtime(&t)->tm_wday == 0 /*Sunday*/)
                     ChatHandler(player->GetSession()).PSendSysMessage("The weekend bonus is active, doubling the experience and reputation received!");
         }
+
+    private:
+        time_t t = time(NULL);
 };
 
 void AddWeekendBonusScripts()
