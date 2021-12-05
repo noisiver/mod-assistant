6cadd670bb57461cb98debbb74204dc5d34370b8
Revision <tkn963@gmail.com>
2021-12-05 13:25:22 +0100

    Fix
    I'm testing this to see if the server will change the day without a restart. The previous attempt didn't work.

--- src/WeekendBonus.cpp
+++ src/WeekendBonus.cpp
@@ -11,26 +11,35 @@ class WeekendBonus : public PlayerScript
         void OnGiveXP(Player* player, uint32& amount, Unit* /*victim*/) override
         {
             if (sConfigMgr->GetBoolDefault("Assistant.Rate.Weekend.Enabled", 0))
+            {
+                time_t t = time(NULL);
+
                 if (localtime(&t)->tm_wday == 5 /*Friday*/ || localtime(&t)->tm_wday == 6 /*Saturday*/ || localtime(&t)->tm_wday == 0 /*Sunday*/)
                     amount *= 2;
+            }
         }
 
         void OnReputationChange(Player* player, uint32 /*factionId*/, int32& standing, bool /*incremental*/) override
         {
             if (sConfigMgr->GetBoolDefault("Assistant.Rate.Weekend.Enabled", 0))
+            {
+                time_t t = time(NULL);
+
                 if (localtime(&t)->tm_wday == 5 /*Friday*/ || localtime(&t)->tm_wday == 6 /*Saturday*/ || localtime(&t)->tm_wday == 0 /*Sunday*/)
                     standing *= 2;
+            }
         }
 
         void OnLogin(Player* player) override
         {
             if (sConfigMgr->GetBoolDefault("Assistant.Rate.Weekend.Enabled", 0))
+            {
+                time_t t = time(NULL);
+
                 if (localtime(&t)->tm_wday == 5 /*Friday*/ || localtime(&t)->tm_wday == 6 /*Saturday*/ || localtime(&t)->tm_wday == 0 /*Sunday*/)
                     ChatHandler(player->GetSession()).PSendSysMessage("The weekend bonus is active, doubling the experience and reputation received!");
+            }
         }
-
-    private:
-        time_t t = time(NULL);
 };
 
 void AddWeekendBonusScripts()
