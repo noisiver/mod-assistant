f5d0af7f5e888b7b20db2bb3c1417f5d565e024e
Revision <tkn963@gmail.com>
2021-12-06 01:41:34 +0100

    Flight form
    Added Flight Form and Swift Flight Form to Druid.

--- sql/world/base/mod_assistant.sql
+++ sql/world/base/mod_assistant.sql
@@ -1766,6 +1766,7 @@ INSERT INTO `assistant_spells` (`type`, `race_id`, `class_id`, `spell_id`, `requ
 INSERT INTO `assistant_spells` (`type`, `race_id`, `class_id`, `spell_id`, `required_level`, `required_spell_id`, `comment`) VALUES (0, -1, 11, 31018, 60, 22829, 'Ferocious Bite (Rank 5)');
 INSERT INTO `assistant_spells` (`type`, `race_id`, `class_id`, `spell_id`, `required_level`, `required_spell_id`, `comment`) VALUES (0, -1, 11, 31709, 60, 9892, 'Cower (Rank 4)');
 INSERT INTO `assistant_spells` (`type`, `race_id`, `class_id`, `spell_id`, `required_level`, `required_spell_id`, `comment`) VALUES (0, -1, 11, 50765, 60, 50766, 'Revive (Rank 5)');
+INSERT INTO `assistant_spells` (`type`, `race_id`, `class_id`, `spell_id`, `required_level`, `required_spell_id`, `comment`) VALUES (0, -1, 11, 33943, 60, 34090, 'Flight Form (Shapeshift)');
 INSERT INTO `assistant_spells` (`type`, `race_id`, `class_id`, `spell_id`, `required_level`, `required_spell_id`, `comment`) VALUES (0, -1, 11, 26984, 61, 9912, 'Wrath (Rank 9)');
 INSERT INTO `assistant_spells` (`type`, `race_id`, `class_id`, `spell_id`, `required_level`, `required_spell_id`, `comment`) VALUES (0, -1, 11, 27001, 61, 9830, 'Shred (Rank 6)');
 INSERT INTO `assistant_spells` (`type`, `race_id`, `class_id`, `spell_id`, `required_level`, `required_spell_id`, `comment`) VALUES (0, -1, 11, 22570, 62, -1, 'Maim (Rank 1)');
@@ -1803,6 +1804,7 @@ INSERT INTO `assistant_spells` (`type`, `race_id`, `class_id`, `spell_id`, `requ
 INSERT INTO `assistant_spells` (`type`, `race_id`, `class_id`, `spell_id`, `required_level`, `required_spell_id`, `comment`) VALUES (0, -1, 11, 27002, 70, 27001, 'Shred (Rank 7)');
 INSERT INTO `assistant_spells` (`type`, `race_id`, `class_id`, `spell_id`, `required_level`, `required_spell_id`, `comment`) VALUES (0, -1, 11, 27012, 70, 17402, 'Hurricane (Rank 4)');
 INSERT INTO `assistant_spells` (`type`, `race_id`, `class_id`, `spell_id`, `required_level`, `required_spell_id`, `comment`) VALUES (0, -1, 11, 33786, 70, -1, 'Cyclone');
+INSERT INTO `assistant_spells` (`type`, `race_id`, `class_id`, `spell_id`, `required_level`, `required_spell_id`, `comment`) VALUES (0, -1, 11, 40120, 70, 34091, 'Swift Flight Form (Shapeshift)');
 INSERT INTO `assistant_spells` (`type`, `race_id`, `class_id`, `spell_id`, `required_level`, `required_spell_id`, `comment`) VALUES (0, -1, 11, 48442, 71, 26980, 'Regrowth (Rank 11)');
 INSERT INTO `assistant_spells` (`type`, `race_id`, `class_id`, `spell_id`, `required_level`, `required_spell_id`, `comment`) VALUES (0, -1, 11, 48559, 71, 26998, 'Demoralizing Roar (Rank 7)');
 INSERT INTO `assistant_spells` (`type`, `race_id`, `class_id`, `spell_id`, `required_level`, `required_spell_id`, `comment`) VALUES (0, -1, 11, 49799, 71, 27008, 'Rip (Rank 8)');
